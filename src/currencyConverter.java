46bba8bfc9d8dc7cd4762337fd9b5d8d2ee1732b
dream-fyre <nery.rivas2017@gmail.com>
2022-09-18 00:11:58 -0400

    initial commit

---
+++ src/currencyConverter.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class currencyConverter {
+    private JButton convertButton;
+    private JTextField textAmount;
+    private JComboBox txtFrom;
+    private JComboBox txtTo;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
